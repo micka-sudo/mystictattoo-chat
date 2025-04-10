c2bc9c89e6ff3a9d8ec21ac8eeec088f72a1a499
micka-sudo <mickael.gerard.dev@gmail.com>
2025-04-10 11:40:27 +0200

    modif footer css+ add socialnetwork js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,47 @@
-// src/components/Footer.js
 import React from 'react';
-import './Footer.scss'; // Fichier SCSS dédié
+import './Footer.scss';
 
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer__content">
-                <p>&copy; 2025 Mystic Tattoo. Tous droits réservés.</p>
+            <div className="footer__container">
+                <p className="footer__address">
+                    📍 19 Boulevard Jean Jaurès, 54000 Nancy
+                </p>
+
+                <div className="footer__newsletter">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            alert("Fonctionnalité d'envoi à connecter !");
+                        }}
+                    >
+                        <input
+                            type="email"
+                            id="newsletter"
+                            placeholder="Votre email"
+                            required
+                        />
+                        <button type="submit">S'inscrire</button>
+                    </form>
+                </div>
+
+                <div className="footer__links">
+                    <a
+                        href="https://www.instagram.com/directory.nancy.tattoo.artists/p/CvKA3RAri-q/?locale=ne_NP"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Instagram
+                    </a>
+                    <a
+                        href="https://www.facebook.com/p/Mystic-Tattoo-Nancy-100057617876652/?locale=fr_FR"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Facebook
+                    </a>
+                </div>
             </div>
         </footer>
     );
